111a6cdda63b25ec2d743059be5d29311bbd7ea0
JulioCHBasso <juliohonoratobasso@gmail.com>
2024-05-06 11:13:46 -0300

    configuração do back-end de curtidas

--- client/rede-social/src/components/Post.tsx
+++ client/rede-social/src/components/Post.tsx
@@ -24,28 +24,71 @@ interface IComments {
     post_id: number;
     created_at: string;
 }
+interface ILikes {
+    id: number;
+    likes_user_id: number;
+    username: string;
+    likes_post_id: number;
+}
+
 function Post(props: { post: IPost }) {
     const { post_desc, img, username, user_img, created_at, id } = props.post
-
     const { user } = useContext(UserContext);
     const queryClient = useQueryClient();
-
     const [comment_desc, setComment_desc] = useState('');
     const [showComments, setShowComments] = useState(false);
-
-    const { data, error, isLoading } = useQuery<IComments[] | undefined>({
+    const [liked, setLiked] = useState(false);
+    const [showlikes, setShowLikes] = useState(false);
+    //likes
+    const likesQuery = useQuery<ILikes[] | undefined>({
+        queryKey: ['likes', id],
+        queryFn: () => makeRequest.get('likes/?likes_post_id=' + id).then((res) => {
+            res.data.data.map((like: ILikes) => {
+                if (like.likes_user_id === user?.id) {
+                    return setLiked(true);
+                } else {
+                    setLiked
+                }
+            })
+            return res.data.data;
+        }),
+        enabled: !!id
+    })
+    if (likesQuery.error) {
+        console.debug(likesQuery.error);
+    }
+    const likesMutation = useMutation({
+        mutationFn: async (newLikes: {}) => {
+            if (liked) {
+                await makeRequest.delete(`likes/?likes_post_id=${id}&likes_user_id=${user?.id}`, newLikes).then((res) => {
+                    setLiked(false)
+                    return res.data;
+                });
+            } else {
+                await makeRequest.post('likes/', newLikes).then((res) => {
+                    return res.data;
+                });
+            }
+        },
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ["likes", id] })
+        },
+    });
+    const shareLikes = async () => {
+        likesMutation.mutate({ likes_user_id: user?.id, likes_post_id: id });
+    };
+    //comentarios
+    const commentsQuery = useQuery<IComments[] | undefined>({
         queryKey: ['comments', id],
         queryFn: () => makeRequest.get('comment/?post_id=' + id).then((res) => {
             return res.data.data;
         }),
         enabled: !!id
-
     })
-    if (error) {
-        console.debug(error);
+    if (commentsQuery.error) {
+        console.debug(commentsQuery.error);
     }
-
-    const mutation = useMutation({
+    const commentsMutation = useMutation({
         mutationFn: async (newComment: {}) => {
             await makeRequest.post('comment/', newComment).then((res) => {
                 return res.data;
@@ -53,20 +96,13 @@ function Post(props: { post: IPost }) {
         },
         onSuccess: () => {
             queryClient.invalidateQueries({ queryKey: ["comments", id] })
-
-
         },
     });
-
     const shareComment = async () => {
-        mutation.mutate({ comment_desc, comment_user_id: user?.id, post_id: id });
+        commentsMutation.mutate({ comment_desc, comment_user_id: user?.id, post_id: id });
         setComment_desc("");
-
     }
     const userImgSrc = user?.user_img ?? "https://img.freepik.com/free-icon/user_318-159711.jpg";
-
-
-
     return (
         <div className="w-1/3 bg-white wounded-lg p-4 shadow-md">
             <header className="flex gap-2 pb-4 border-2 items-center">
@@ -84,18 +120,37 @@ function Post(props: { post: IPost }) {
             )}
             {img && <img className="rounded-lg" src={`./upload/${img}`} alt=" imagem do post" />}
             <div className="flex justify-between py-4 border-b">
-                <div className=" flex gap-1 items-center">
-                    <span className="bg-blue-600 w6 h-6 text-white flex items-center justify-center rounded-full text-xs"><FaThumbsUp />3</span>
+                <div
+                    className="relative"
+                    onMouseEnter={() => setShowLikes(true)}
+                    onMouseLeave={() => setShowLikes(false)}>
+                    {likesQuery.data && likesQuery.data.length > 0 && (
+                        <>
+                            <div className=" flex gap-1 items-center"
+                            >
+                                <span className="bg-blue-600 w6 h-6 text-white flex items-center justify-center rounded-full text-xs">
+                                    <FaThumbsUp /></span>
+                                <span>{likesQuery.data.length}</span>
+                            </div>
+                            {showlikes && (
+                                <div className="absolute bg-white border flex fex-col p2 rounded-md top-6">
+                                    {likesQuery.data.map((like) => {
+                                        return <span key={like.id}>{like.username}</span>
+                                    })}
+                                </div>
+                            )}
+                        </>
+                    )}
                 </div>
-                <button onClick={()=>setShowComments(!showComments)}>{data && data.length > 0 ? `${data?.length} comentarios` : ""} </button>
+                <button onClick={() => setShowComments(!showComments)}>{commentsQuery.data && commentsQuery.data.length > 0 && `${commentsQuery.data?.length} comentarios`} </button>
             </div>
             <div className=" flex justify-between py-4  text-gray-600 border-b">
-                <button className="flex items-center gap-1"><FaThumbsUp /> Curtir</button>
-                <button className="flex items-center gap-1" onClick={()=> document.getElementById("comment"+id)?.focus()}><FaRegComment /> Comentar</button>
+                <button className={`flex items-center gap-1 ${liked ? "text-blue-600" : ""}`} onClick={() => shareLikes()}><FaThumbsUp /> Curtir</button>
+                <button className="flex items-center gap-1" onClick={() => document.getElementById("comment" + id)?.focus()}><FaRegComment /> Comentar</button>
             </div>
-            {showComments && data?.map((comment,id)=>{
-                    return <Comments comment = {comment}key={id}/>
-                })}
+            {showComments && commentsQuery.data?.map((comment, id) => {
+                return <Comments comment={comment} key={id} />
+            })}
 
             <div className="flex gap-4 pt-6">
                 <img src={userImgSrc}
@@ -103,11 +158,11 @@ function Post(props: { post: IPost }) {
                     className="w-8 h-8 rounded-full"
                 />
 
-               
+
                 <div className="w-full bg-zinc-100 items-center text-gray-600 px-3 py-1 rounded-full">
                     <input
-                    id={"comment"+id}
-                     type="text" className="bg-zinc-100 w-full focus-visible:outline-none"
+                        id={"comment" + id}
+                        type="text" className="bg-zinc-100 w-full focus-visible:outline-none"
                         value={comment_desc}
                         onChange={(e) => setComment_desc(e.target.value)}
                         placeholder="Comente..." />
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import userRouter from './routes/user.js';
 import authRouter from './routes/auth.js';
 import postRouter from './routes/post.js';
+import likesRouter from './routes/likes.js';
 import commentRouter from './routes/comment.js';
 import uploadRouter from './routes/upload.js';
 import bodyParser from "body-parser";
@@ -35,6 +36,7 @@ app.use(cookieParser());
 app.use('/api/users/', userRouter);
 app.use('/api/auth/', authRouter);
 app.use('/api/post/',postRouter);
+app.use('/api/likes/',likesRouter);
 app.use('/api/comment/',commentRouter);
 app.use('/api/upload/',uploadRouter);
 
