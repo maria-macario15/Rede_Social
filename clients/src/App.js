f0e3ed93b9ecdff92d2afae6a592ae85df86f667
maria-macario15 <marialuizamacario00@gmail.com>
2024-06-20 00:29:54 -0300

    atualização

--- clients/src/App.js
+++ clients/src/App.js
@@ -1,26 +1,31 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Login from "./views/auth/login"
+import { Routes, Route, Outlet, useNavigate } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { useEffect } from 'react';
-import Caminho from "./caminho"
-import inicial from './views/components/inicial'
-import Barra from './views/components/Barra';
-import fim from './views/components/Final';
-function App() {
-  useEffect(() => {
-    document.title = "Taruira Chapoca"
+import Login from "./views/auth/login";
+import Profile from './views/Profile/index';
 
-  }, [])
-
-  return (
-    <BrowserRouter>
-    <Barra/>
-      <Routes>
-        <Route path='/' element={<inicial/>} /> 
-      
-      </Routes>
+function ProtectedLayout() {
+  const navigate = useNavigate();
   
-    </BrowserRouter>
+  useEffect(() => {
+    document.title = "Taruira Chapoca";
+    let value = localStorage.getItem("rede-social:token");
+    if (!value) {
+      navigate('/login'); // Redirects to the '/login' route if no token is found
+    }
+  }, [navigate]);
+
+  return <Outlet />;
+}
+
+function App() {
+  return (
+    <Routes>
+      <Route element={<ProtectedLayout />}>
+        <Route path="/" element={<Profile />} />
+      </Route>
+      <Route path="/login" element={<Login />} />
+    </Routes>
   );
 }
 
---
+++ clients/src/views/Profile/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Barra from '../components/Barra';
+
+const Profile = ({ children }) => (
+    <>
+        <Barra />
+        <div className="main-content">
+            {children}
+        </div>
+    </>
+);
+
+export default Profile;
